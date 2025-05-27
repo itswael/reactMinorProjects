963ad119d802b12e7d57705957c644411974ef20
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-27 16:19:58 -0400

    added button toggle using useState

--- 02-ticTacToe/src/components/Player.jsx
+++ 02-ticTacToe/src/components/Player.jsx
@@ -1,11 +1,17 @@
+import {useState} from "react";
 export default function Player({name, symbol}){
+    const [isEditing, setIsEditing] = useState(false);
+    function handleButton(){
+        setIsEditing(editing => !editing);
+    }
+    let buttonText = isEditing?"Save":"Edit"
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {!isEditing? <span className="player-name">{name}</span>:<input type="text" defaultValue={name}/>}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleButton}>{buttonText}</button>
         </li>
     );
 }
