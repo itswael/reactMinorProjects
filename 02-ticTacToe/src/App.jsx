4f95676ac5a481ce42495bbca9c17df7e5009384
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-27 19:43:16 -0400

    added active player hook to switch between the players

--- 02-ticTacToe/src/App.jsx
+++ 02-ticTacToe/src/App.jsx
@@ -1,14 +1,20 @@
+import {useState} from "react";
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X');
+    function handleSelectSquare(){
+        setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initName={"player 1"} symbol={"X"}/>
-          <Player initName={"player 2"} symbol={"O"}/>
+        <ol id="players" className="highlight-player">
+          <Player initName={"player 1"} symbol={"X"} isActive={activePlayer === 'X'}/>
+          <Player initName={"player 2"} symbol={"O"} isActive={activePlayer === 'O'}/>
         </ol>
-        GAME BOARD
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayer={activePlayer} />
       </div>
     </main>
   )
