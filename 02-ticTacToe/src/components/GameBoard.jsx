afc06385460a89577e5b613939e3c54be65bf399
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-29 18:36:17 -0400

    modified gameBoard to have gameboard and updating the board in App component

--- 02-ticTacToe/src/components/GameBoard.jsx
+++ 02-ticTacToe/src/components/GameBoard.jsx
@@ -1,11 +1,4 @@
-import {useState} from "react";
-
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
-export default function GameBoard({onSelectSquare, turns}) {
+export default function GameBoard({onSelectSquare, gameBoard}) {
     // const [gameBoard, setGameBoard] = useState(initialGameBoard)
     // function handleSelectSquare(rowIndex, colIndex){
     //     setGameBoard((prevGameBoard) => {
@@ -17,12 +10,6 @@ export default function GameBoard({onSelectSquare, turns}) {
     //     onSelectSquare()
     // }
 
-    let gameBoard = initialGameBoard;
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-        gameBoard[row][col] = player;
-    }
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => (
