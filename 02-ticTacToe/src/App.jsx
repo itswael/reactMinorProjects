1e9cebed9fadeb884815dae5a1f33bd44318e632
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-29 18:36:51 -0400

    added functionality to update gameboard

--- 02-ticTacToe/src/App.jsx
+++ 02-ticTacToe/src/App.jsx
@@ -10,6 +10,33 @@ function deriveActivePlayer(gameTurns){
     }
     return currentPlayer
 }
+
+function updateGameBoard(gameTurns){
+    let gameBoard = [...initialGameBoard.map(row => [...row])];
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+        gameBoard[row][col] = player;
+    }
+
+    return gameBoard;
+}
+
+function findWinner(gameBoard){
+    let winner;
+    for (const combination of WINNING_COMBINATIONS){
+        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+        if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && secondSquareSymbol === thirdSquareSymbol){
+            winner = firstSquareSymbol;
+        }
+    }
+
+    return winner;
+}
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
     //const [activePlayer, setActivePlayer] = useState('X');
