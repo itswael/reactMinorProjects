c4f66300338a622c38cb9cd4a1776704ef9dcdbe
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-29 15:08:21 -0400

    handling the game board logic for moves in parent component (App), utilizing the same state for the in game logs

--- 02-ticTacToe/src/App.jsx
+++ 02-ticTacToe/src/App.jsx
@@ -1,11 +1,25 @@
 import {useState} from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
 function App() {
+    const [gameTurns, setGameTurns] = useState([]);
     const [activePlayer, setActivePlayer] = useState('X');
-    function handleSelectSquare(){
+
+    function handleSelectSquare(rowIndex, colIndex) {
         setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+        setGameTurns((prevTurns) => {
+            let currentPlayer = 'X';
+            if (prevTurns.length > 0 && prevTurns[0].player == 'X'){
+                currentPlayer = 'O';
+            }
+            const updatedTurns = [{
+                square: { row: rowIndex, col: colIndex },player: currentPlayer,},
+                ...prevTurns
+            ]
+            return updatedTurns
+        })
     }
   return (
     <main>
@@ -14,7 +28,10 @@ function App() {
           <Player initName={"player 1"} symbol={"X"} isActive={activePlayer === 'X'}/>
           <Player initName={"player 2"} symbol={"O"} isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayer={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+      </div>
+      <div>
+        <Log gameTurns={gameTurns} />
       </div>
     </main>
   )
