37c792941c232aaec43d8c92ce36896ac57728a7
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-29 15:39:06 -0400

    deriving current player from gameTurns

--- 02-ticTacToe/src/components/Log.jsx
+++ 02-ticTacToe/src/components/Log.jsx
@@ -1,7 +1,7 @@
 export default function Log({gameTurns}) {
     return (<ol id="log">
         {gameTurns.map(turn =>
-            <li>{turn.player}</li>
+            <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row},{turn.square.col}</li>
             )}
     </ol>)
 }
