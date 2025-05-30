eee13993b322eb399fadea61ec231602b9458180
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-30 01:04:21 -0400

    modified with lifted up state, and added results table

--- 03-InvestmentCalculator/src/App.jsx
+++ 03-InvestmentCalculator/src/App.jsx
@@ -1,6 +1,32 @@
+import Header from './components/Header.jsx'
+import UserInput from "./components/UserInput.jsx";
+import {useState} from "react";
+import Results from "./components/Results.jsx";
 function App() {
+  const [userInput, setUserInput] =useState({
+    initialInvestment:1000,
+    annualInvestment:1200,
+    expectedReturn:6,
+    duration:10
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      }
+    });
+  }
+
+  const inputIsValid = userInput["duration"]>0
   return (
-    <h1>React Investment Calculator</h1>
+      <>
+        <Header/>
+        <UserInput handleChange={handleChange} userInput={userInput} />
+        {!inputIsValid && <p className="center">Please enter a duration greater than 0</p>}
+        {inputIsValid && <Results input={userInput} />}
+      </>
   )
 }
 
