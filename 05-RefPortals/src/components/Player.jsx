ab1a328b22866f5ec8648932a6646cc17e5e21ba
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-04 22:29:18 -0400

    used useState to update the name

--- 05-RefPortals/src/components/Player.jsx
+++ 05-RefPortals/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import {useState} from "react";
+
 export default function Player() {
+    const [name, setName] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    function handleNameChange(evt) {
+        setName(evt.target.value);
+    }
+
+    function handleClick(){
+        setSubmitted(true);
+    }
     return (
         <section id="player">
-            <h2>Welcome unknown entity</h2>
+            <h2>Welcome {submitted && name}</h2>
             <p>
-                <input type="text" />
-                <button>Set Name</button>
+                <input type="text" onChange={handleNameChange} />
+                <button onClick={handleClick}>Set Name</button>
             </p>
         </section>
     )
