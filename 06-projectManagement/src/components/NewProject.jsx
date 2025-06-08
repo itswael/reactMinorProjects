0adabd0337229db63340fec8b104b7a3741591d2
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-08 16:33:57 -0400

    added ref to input fields

--- 06-projectManagement/src/components/NewProject.jsx
+++ 06-projectManagement/src/components/NewProject.jsx
@@ -1,6 +1,11 @@
 import Input from "./Input.jsx";
+import {useRef} from "react";
 
 export default function NewProject(){
+    const title = useRef()
+    const description = useRef()
+    const dueDate = useRef()
+
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
@@ -8,9 +13,9 @@ export default function NewProject(){
                 <li><button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
-                <Input label={"Title"} textArea={false}/>
-                <Input label={"Description"} textArea={true}/>
-                <Input label={"Due Date"} textArea={false}/>
+                <Input ref={title} label={"Title"} textArea={false}/>
+                <Input ref={description} label={"Description"} textArea={true}/>
+                <Input ref={dueDate} label={"Due Date"} textArea={false}/>
             </div>
         </div>
     )
