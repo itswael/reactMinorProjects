7e4531821562e05cbbd3ba0e079725c448472a3d
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-08 16:07:47 -0400

    using Button component for the button

--- 06-projectManagement/src/components/NoProjectSelected.jsx
+++ 06-projectManagement/src/components/NoProjectSelected.jsx
@@ -1,4 +1,5 @@
 import noProjectImage from '../assets/no-projects.png'
+import Button from "./Button.jsx";
 export default function NoProjectSelected() {
     return (
         <div className={"mt-24 text-center w-2/3"}>
@@ -6,7 +7,7 @@ export default function NoProjectSelected() {
             <h2 className={"text-xl font-bold text-stone-500 my-4"}> No Project Selected</h2>
             <p className={"text-stone-400 mb-4"}>Select a project or get started with a new one</p>
             <p className={"mt-8"}>
-                <button>Create new project</button>
+                <Button>Create new project</Button>
             </p>
         </div>
     )
