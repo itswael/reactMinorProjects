a315719f6e4b3bf7e3b734c846b15e78c85a5890
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-08 16:56:30 -0400

    added a function to build a new project object with the input values and call add method from app component

--- 06-projectManagement/src/components/NewProject.jsx
+++ 06-projectManagement/src/components/NewProject.jsx
@@ -1,21 +1,36 @@
 import Input from "./Input.jsx";
 import {useRef} from "react";
 
-export default function NewProject(){
+export default function NewProject({onAdd}){
     const title = useRef()
     const description = useRef()
     const dueDate = useRef()
+
+    function handleSave(){
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate,
+        })
+
+    }
 
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+                <li><button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950"
+                onClick={handleSave}>
+                    Save</button></li>
             </menu>
             <div>
                 <Input ref={title} label={"Title"} textArea={false}/>
                 <Input ref={description} label={"Description"} textArea={true}/>
-                <Input ref={dueDate} label={"Due Date"} textArea={false}/>
+                <Input type="date" ref={dueDate} label={"Due Date"} textArea={false}/>
             </div>
         </div>
     )
