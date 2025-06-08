0abc87cd25af45600c6159ed2c1f93d3ffd2f329
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-07 23:22:32 -0400

    adding displaying remaining time

--- 05-RefPortals/src/components/ResultModal.jsx
+++ 05-RefPortals/src/components/ResultModal.jsx
@@ -1,6 +1,6 @@
 import {useRef, useImperativeHandle} from "react";
 
-export default function ResultModal({ref, result, targetTime}) {
+export default function ResultModal({ref, remainingTime, onClose, targetTime}) {
     const dialog = useRef();
     useImperativeHandle(ref, () => {
         return {
@@ -11,10 +11,10 @@ export default function ResultModal({ref, result, targetTime}) {
     })
     return (
         <dialog ref = {dialog} className="result-modal">
-            <h2>You {result}</h2>
+            <h2>You {remainingTime>0? "Won" : "Lost"}</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with X seconds left.</p>
-            <form method="dialog">
+            <p>You stopped the timer with {(remainingTime/1000).toFixed(2)} seconds left.</p>
+            <form method="dialog" onSubmit={onClose}>
                 <button>Close</button>
             </form>
         </dialog>
