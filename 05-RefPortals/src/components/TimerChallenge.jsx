bd18ede676126390eadbc7f658d68a5795b129a8
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-07 22:48:17 -0400

    passing refs to condition display the modal

--- 05-RefPortals/src/components/TimerChallenge.jsx
+++ 05-RefPortals/src/components/TimerChallenge.jsx
@@ -5,8 +5,12 @@ export default function TimerChallenge({title, targetTime}) {
     const [timerExpired, setTimerExpired] = useState(false);
     const [timerStarted, setTimerStarted] = useState(false);
     const timer = useRef();
+    const dialog = useRef();
     function handleStart(){
-        timer.current = setTimeout(() => {setTimerExpired(true)}, targetTime * 1000);
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+            dialog.current.showModal();
+        }, targetTime * 1000);
         setTimerStarted(true);
     }
     function handleStop(){
@@ -14,7 +18,7 @@ export default function TimerChallenge({title, targetTime}) {
     }
     return (
         <>
-            {timerExpired && <ResultModal targetTime={targetTime} result={"Lost"}/>}
+            <ResultModal ref = {dialog} targetTime={targetTime} result={"Lost"}/>
             <section className="challenge">
                 <h2>{title}</h2>
                 {timerExpired && <p>You lost</p>}
