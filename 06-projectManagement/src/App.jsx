beb89db5ac61ee3fbb9ce7ce1dcacbf1ccc09371
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-08 23:13:48 -0400

    added function to delete selected project

--- 06-projectManagement/src/App.jsx
+++ 06-projectManagement/src/App.jsx
@@ -37,6 +37,16 @@ function App() {
         })
     }
 
+    function handleDeleteProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId)
+            }
+        })
+    }
+
     function handleAddProject(projectData) {
         setProjectsState(prevState => {
             const projectId = Math.random()
@@ -53,7 +63,7 @@ function App() {
     }
     const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-    let content = <SelectedProject project={selectedProject} />;
+    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
     if (projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
