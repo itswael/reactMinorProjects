12136d9a51f55ee7ac145fda62944c5e21ee0ba5
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-10 20:38:49 -0400

    prop drilling for adding and deleting tasks

--- 06-projectManagement/src/components/Tasks.jsx
+++ 06-projectManagement/src/components/Tasks.jsx
@@ -1,10 +1,10 @@
 import NewTask from "./NewTask.jsx";
 
-export default function Tasks() {
+export default function Tasks({tasks, onDelete, onAdd}) {
     return (
         <section>
             <h2 className={"text-2xl font-bold text-stone-700 mb-4"}>Tasks</h2>
-            <NewTask/>
+            <NewTask onAdd={onAdd} onDelete={onDelete}/>
             <p className={"text-stone-800 my-4"}>
                 No Tasks Available
             </p>
