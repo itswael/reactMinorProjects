1e8833578f886bf1c340a21446c91b3d3b341e63
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-10 21:33:45 -0400

    added method to add tasks

--- 06-projectManagement/src/App.jsx
+++ 06-projectManagement/src/App.jsx
@@ -7,8 +7,29 @@ import SelectedProject from "./components/selectedProject.jsx";
 function App() {
     const [projectsState, setProjectsState] = useState({
         selectedProjectId: undefined,
-        projects:[]
+        projects:[],
+        tasks: []
     })
+
+    function handleAddTask(text){
+        setProjectsState(prevState => {
+            const taskId = Math.random()
+            const newTask = {
+                text: text,
+                projectId: prevState.projectId,
+                id: taskId
+            }
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                task: [newTask, ...prevState.tasks]
+            }
+        })
+    }
+
+    function handleDeleteTask(){
+
+    }
 
     function handleStartAddProject() {
         setProjectsState(prevState => {
@@ -63,7 +84,10 @@ function App() {
     }
     const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+    let content = <SelectedProject  onAddTask = {handleAddTask}
+                                    onDeleteTask={handleDeleteTask}
+                                    project={selectedProject}
+                                    onDelete={handleDeleteProject}/>;
 
     if (projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
