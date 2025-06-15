1ace8a539f9da55b072974b75a4968c60efbe815
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-14 22:26:47 -0400

    added callback hook, to stop creating a new function on component reload

--- 09-quizApp/src/components/Quiz.jsx
+++ 09-quizApp/src/components/Quiz.jsx
@@ -1,15 +1,18 @@
-import {useState} from "react";
+import {useState, useCallback} from "react";
 import QUESTIONS from "../questions.js"
 import QuizCompleteLogo from "../assets/quiz-complete.png"
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz(){
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer){
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
         setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
-    }
+    }, [])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
         return <div id={"summary"}>
@@ -26,6 +29,7 @@ export default function Quiz(){
     return (
         <div id={"quiz"}>
             <div id={"question"}>
+                <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}/>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id={"answers"}>
                     {shuffledAnswers.map((answer) => (
