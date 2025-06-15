73d9f6e8de1823f555216f46b054853c9f487728
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-15 16:16:56 -0400

    added index prop for the current question

--- 09-quizApp/src/components/Quiz.jsx
+++ 09-quizApp/src/components/Quiz.jsx
@@ -1,32 +1,16 @@
-import {useState, useRef, useCallback} from "react";
+import {useState, useCallback} from "react";
 import QUESTIONS from "../questions.js"
 import QuizCompleteLogo from "../assets/quiz-complete.png"
-import QuestionTimer from "./QuestionTimer.jsx";
-import Answers from "./Answers.jsx";
 import Question from "./Question.jsx";
 
 export default function Quiz(){
-    const [answerState, setAnswerState] = useState('')
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length-1
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
-        setAnswerState('Answered')
         setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
-                setAnswerState('correct')
-            }else{
-                setAnswerState('wrong')
-            }
-
-            setTimeout(() => {
-                setAnswerState('');
-            }, 2000)
-
-        }, 1000)
-    }, [activeQuestionIndex])
+    }, [])
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -40,10 +24,8 @@ export default function Quiz(){
     return (
         <div id={"quiz"}>
             <Question key={activeQuestionIndex}
-                      questionText={QUESTIONS[activeQuestionIndex].text}
-                      answers={QUESTIONS[activeQuestionIndex].answers}
+                      index={activeQuestionIndex}
                       onSelectAnswer={handleSelectAnswer}
-                      selectedAnswer={userAnswers[userAnswers.length -1]}
                       onSkipAnswer={handleSkipAnswer} />
         </div>
     )
