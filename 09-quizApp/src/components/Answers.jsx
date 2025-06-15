6b09c9fc4fd1a7fd0b50da3e0711ecd0b7ee24f8
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-15 14:32:26 -0400

    added Answers component to easy shuffle and update answer options based on the question

---
+++ 09-quizApp/src/components/Answers.jsx
@@ -0,0 +1,28 @@
+import QUESTIONS from "../questions.js";
+import {useRef} from "react";
+
+export default function Answers({answers, selectedAnswers, answerState, onSelect}){
+    const shuffledAnswers = useRef()
+
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...answers]
+        shuffledAnswers.current.sort((a, b) => Math.random() - 0.5)
+    }
+    return <ul id={"answers"}>
+        {shuffledAnswers.current.map((answer) => {
+            const isSelected = selectedAnswers === answer;
+            let cssClasses = '';
+            if (answerState === 'answered' && isSelected) {
+                cssClasses = ' selected';
+            }
+            if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                cssClasses = answerState;
+            }
+            return <li key={answer} className={"answer"}>
+                <button onClick={() => onSelect(answer)} className={cssClasses} >
+                    {answer}
+                </button>
+            </li>
+        })}
+    </ul>
+}
