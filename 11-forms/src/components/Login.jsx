3aa27e3eba5bdcebd62643c2805e58aa406fb122
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-26 06:01:27 -0400

    handling values as an combined object

--- 11-forms/src/components/Login.jsx
+++ 11-forms/src/components/Login.jsx
@@ -1,7 +1,22 @@
+import {useState} from "react";
+
 export default function Login() {
+    const [enteredValues, setEnteredValues] = useState({
+        email: "",
+        password: "",
+    })
+
+    function handleInputChange(identifier, event){
+        setEnteredValues((prevState) => ({
+            ...prevState,
+                [identifier]: event.target.value
+        }))
+    }
+
     function handleSubmit(event){
         event.preventDefault();
     }
+
   return (
     <form onClick={handleSubmit}>
       <h2>Login</h2>
@@ -9,12 +24,20 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email"
+                 type="email"
+                 name="email"
+                 onChange={(event) => handleInputChange("email", event)}
+                 value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password"
+                 type="password"
+                 name="password"
+                 onChange={(event) => handleInputChange("password", event)}
+                 value={enteredValues.password}/>
         </div>
       </div>
 
