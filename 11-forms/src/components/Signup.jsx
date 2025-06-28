e2b14ac48913e37ae58bbfdc135d4ff5ce65850a
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-28 13:55:26 -0400

    added data fetching logic to form on signup page

--- 11-forms/src/components/Signup.jsx
+++ 11-forms/src/components/Signup.jsx
@@ -1,6 +1,12 @@
 export default function Signup() {
+    function handleSubmit(event){
+        event.preventDefault();
+        const form = new FormData(event.target)
+        const acquisition = form.getAll("Acquisition")
+        const data = Object.fromEntries(form.entries())
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Welcome on board!</h2>
             <p>We just need a little bit of data from you to get you started 🚀</p>
 
