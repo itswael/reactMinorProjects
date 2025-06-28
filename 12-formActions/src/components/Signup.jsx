4f94b8cb21e7d170b3521b5568f65f59e604246a
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-28 18:11:42 -0400

    maintaining the form state in case of failed submission

--- 12-formActions/src/components/Signup.jsx
+++ 12-formActions/src/components/Signup.jsx
@@ -43,7 +43,17 @@ export default function Signup() {
         }
 
         if(errors.length > 0){
-            return {errors};
+            return {errors,
+            enteredValues: {
+                firstName,
+                lastName,
+                role,
+                enteredEmail,
+                enteredPassword,
+                confirmPassword,
+                acquisition,
+                terms
+            }};
         }
 
         return {errors: null}
@@ -58,13 +68,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email} />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" defaultValue={formState.enteredValues?.enteredPassword}/>
         </div>
 
         <div className="control">
@@ -73,6 +83,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -82,18 +93,18 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" defaultValue={formState.enteredValues?.firstName} />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" defaultValue={formState.enteredValues?.lastName} />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" defaultValue={formState.enteredValues?.role}>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -110,6 +121,7 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisition.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -120,12 +132,13 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisition.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input type="checkbox" id="other" name="acquisition" value="other" defaultChecked={formState.enteredValues?.acquisition.includes('other')} />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
