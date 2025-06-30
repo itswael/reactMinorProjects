6ad43973b66ef7e3b1ce32babdc49ee39bf989ff
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-29 23:19:32 -0400

    added component for getting newOpinion from user, fetching data using formActionState

---
+++ 13-asyncFormSubmission/src/components/NewOpinion.jsx
@@ -0,0 +1,69 @@
+import useActionState from 'React'
+export function NewOpinion() {
+  function shareOpinionAction(prevFormState, formData){
+    const title = formData.title;
+    const body = formData.body;
+    const userName = formData.userName;
+
+    let errors = []
+
+    if (title.trim().length < 5){
+      errors.push('Title must be at least 5 characters.');
+    }
+
+    if (body.length < 10 || body.trim().length >300){
+      errors.push('Body must be at least 10 characters.');
+    }
+
+    if  (!userName.trim()){
+      errors.push('Username must be provided.');
+    }
+
+    if (errors.length > 0){
+      return {
+        errors,
+        enteredValues:{
+          title,
+          body,
+          userName,
+        }}
+    }
+
+    return {errors: null}
+
+  }
+
+  const [formState, formAction] = useActionState(shareOpinionAction, {errors: null});
+  return (
+    <div id="new-opinion">
+      <h2>Share your opinion!</h2>
+      <form action={shareOpinionAction}>
+        <div className="control-row">
+          <p className="control">
+            <label htmlFor="userName">Your Name</label>
+            <input type="text" id="userName" name="userName" defaultValue={formState.enteredValues?.username}/>
+          </p>
+
+          <p className="control">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" defaultValue={formState.enteredValues?.title}/>
+          </p>
+        </div>
+        <p className="control">
+          <label htmlFor="body">Your Opinion</label>
+          <textarea id="body" name="body" rows={5} defaultValue={formState.enteredValues?.body}></textarea>
+        </p>
+
+        {formState.errors && <ul className={"errors"}>
+          {formState.errors.map(error => (
+              <li key={error}>{error}</li>
+          ))}
+        </ul>}
+
+        <p className="actions">
+          <button type="submit">Submit</button>
+        </p>
+      </form>
+    </div>
+  );
+}
