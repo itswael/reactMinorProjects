28205441dfa92ed4b0c1cb8f59a7359b9b04c56b
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-01 10:12:13 -0400

    added http methods to upvote and downvote functions

--- 13-asyncFormSubmission/src/store/opinions-context.jsx
+++ 13-asyncFormSubmission/src/store/opinions-context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
+import response from "assert";
 
 export const OpinionsContext = createContext({
   opinions: null,
@@ -37,7 +38,15 @@ export function OpinionsContextProvider({ children }) {
     setOpinions((prevOpinions) => [savedOpinion, ...prevOpinions]);
   }
 
-  function upvoteOpinion(id) {
+  async function upvoteOpinion(id) {
+    const response = await fetch('http://localhost:3000/opinions/' +id+'/upvote', {
+      method: 'POST',
+    })
+
+    if(!response.ok) {
+      return;
+    }
+
     setOpinions((prevOpinions) => {
       return prevOpinions.map((opinion) => {
         if (opinion.id === id) {
@@ -48,7 +57,15 @@ export function OpinionsContextProvider({ children }) {
     });
   }
 
-  function downvoteOpinion(id) {
+  async function downvoteOpinion(id) {
+    const response = await fetch('http://localhost:3000/opinions/' +id+'/downvote', {
+      method: 'POST',
+    })
+
+    if(!response.ok) {
+      return;
+    }
+
     setOpinions((prevOpinions) => {
       return prevOpinions.map((opinion) => {
         if (opinion.id === id) {
